ac6448b60884e42ce88a8952a546426ac6d5d04d
egrgut <egr.gut@gmail.com>
2026-05-23 22:03:15 +0300

    Contact planet API

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,53 @@
+import {useEffect, useState} from "react";
+import {baseUrl, periodMonth} from "../utils/constants.js";
+
 const Contact = () => {
+    const [planets, setPlanets] = useState(() => {
+        const planets = JSON.parse(localStorage.getItem('planets'));
+        if (planets && Date.now() - planets.timestamp < periodMonth) {
+            return planets.payload;
+        } else {
+            return ["loading"];
+        }
+    });
+    useEffect(() => {
+        const getPlanets = () => {
+            fetch(`${baseUrl}/v1/planets`)
+                .then(response => response.json())
+                .then(data => {
+                    const planetsData = data.map(item => item.name);
+                    setPlanets(planetsData);
+                    localStorage.setItem('planets', JSON.stringify({
+                        payload: planets,
+                        timestamp: Date.now()
+                    }));
+                })
+        }
+        getPlanets()
+    },[]);
+
+
     return (
-        <div>
-            Contact
+        <div className="container">
+            <form>
+
+                <label htmlFor="fname">First Name</label>
+                <input type="text" id="fname" name="firstname" placeholder="Your name.."/>
+
+                <label>Last Name</label>
+                <input type="text" id="lname" name="lastname" placeholder="Your last name.."/>
+
+                <label>Planet</label>
+                <select id="planet" name="planet">
+                    {planets.map(item => <option value={item} key={item}>{item}</option>)}
+                </select>
+
+                <label>Subject</label>
+                <textarea id="subject" name="subject" placeholder="Write something.."
+                          style={{height: '200px'}}></textarea>
+
+                <input className="btn btn-danger" type="submit" value="Submit"/>
+            </form>
         </div>
     )
 }
